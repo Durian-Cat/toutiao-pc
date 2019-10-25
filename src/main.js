af0c68375aef47bb41eaf9854184290dcf58bf82
Durian-Cat <1295905426@qq.com>
2019-10-25 19:42:26 +0800

    登录页面完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入文件的时候，默认导入的是目录下的索引文件（index.js index.vue index.json）。
+// @是webpack指定的路径别名  @===‘/src’   绝对路径
+import router from '@/router'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
+// main.js 职责入口文件
+// 职责1：创建根实例
+// 职责2：依赖项目需要的全局资源
