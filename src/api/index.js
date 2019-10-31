77c9b06780e946bacc603cc0d4d78b173993af52
Durian-Cat <1295905426@qq.com>
2019-10-31 21:49:02 +0800

    .

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 import local from '../utils/local'
 import router from '../router'
+import JSONBIG from 'json-bigint'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+axios.defaults.transformResponse = [(data) => {
+  // 对data进行格式转换  data就是后台响应的json字符串
+  // 如果没数据呢？data === null 使用JSONBIG.parse(null) 报错
+  try {
+    return JSONBIG.parse(data)
+  } catch (e) {
+    return data
+  }
+}]
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
